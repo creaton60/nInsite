a9ecb722940c983b6d3c1fc4f2e069be4d8819cc
dino <dino.dk@kakaocorp.com>
2020-01-13 23:33:55 +0900

    [create] project init

---
+++ src/main/java/net/yeolrim/store/insite/InsiteApplication.java
@@ -0,0 +1,13 @@
+package net.yeolrim.store.insite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yeolrim/store/insite/InsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yeolrim.store.insite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
