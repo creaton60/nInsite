3737f520fb17c784e2bfa7f0ed9234f301910139
creaton60 <lovmistake@gmail.com>
2020-01-20 23:37:12 +0900

    [update feature] 불필요한 import 제거

--- src/main/java/net/yeolrim/store/insite/config/MongoConfig.java
+++ src/main/java/net/yeolrim/store/insite/config/MongoConfig.java
@@ -1,10 +1,7 @@
 package net.yeolrim.store.insite.config;
 
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.internal.MongoClientImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
